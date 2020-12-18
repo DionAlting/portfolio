4ec07e3dde259de5b660ebfce274bb3e126ef2ea
DionAlting <dion_a@live.nl>
2020-12-18 03:05:33 +0100

    Develop (#3)
    * Second commit
    
    * Code refactor
    
    * Code refactor
    
    * Some more code refactor
    
    * Fixed img src

--- README.md
+++ README.md
@@ -1,1 +1,80 @@
-# portfolio
+<!-- PROJECT LOGO -->
+<br />
+<p align="center">
+  <a href="https://github.com/DionAlting/portfolio">
+    <img src="./img/Slaklogo.png?raw=true" alt="Logo" width="230" height="180">
+  </a>
+
+  <h3 align="center">Dion Alting</h3>
+
+  <p align="center">
+    Portfolio website
+    <br />
+    <br />
+    <a href="https://thecodingsnail.dev">View Live Page</a>
+  </p>
+</p>
+
+<!-- TABLE OF CONTENTS -->
+<details open="open">
+  <summary><h2 style="display: inline-block">Table of Contents</h2></summary>
+  <ol>
+    <li>
+      <a href="#about-the-project">About The Project</a>
+      <ul>
+        <li><a href="#built-with">Built With</a></li>
+      </ul>
+    </li>
+    <li>
+      <a href="#getting-started">Getting Started</a>
+      <ul>
+        <li><a href="#installation">Installation</a></li>
+      </ul>
+    </li>
+    <li><a href="#usage">Usage</a></li>
+    <li><a href="#contact">Contact</a></li>
+    <li><a href="#acknowledgements">Acknowledgements</a></li>
+  </ol>
+</details>
+
+<!-- ABOUT THE PROJECT -->
+
+## About The Project
+
+I made this portfolio website as part of the first homework assignment from Codaisseur.
+<br><br>_Add image here_
+
+### Built With
+
+- []()HTML
+- []()CSS
+- []()JavaScript
+
+<!-- GETTING STARTED -->
+
+## Getting Started
+
+To get a local copy up and running follow these simple steps.
+
+### Installation
+
+1. Clone the repo
+   ```sh
+   git clone https://github.com/DionAlting/portfolio.git
+   ```
+
+<!-- CONTACT -->
+
+## Contact
+
+LinkedIn - [Dion Alting](https://linkedin.com/in/dion-alting)
+
+Project Link: [https://github.com/DionAlting/portfolio](https://github.com/DionAlting/portfolio)
+
+<!-- ACKNOWLEDGEMENTS -->
+
+## Acknowledgements
+
+- [Font Awesome](https://fontawesome.com)
+- [Normalise.css](https://necolas.github.io/normalize.css/)
+- []()
--- js/index.js
+++ js/index.js
@@ -2,37 +2,39 @@ const themeToggle = document.querySelector(".theme__toggle");
 const theme = document.querySelector(".light");
 const body = document.body;
 
-const localTheme = localStorage.getItem("theme");
+const isDarkMode = localStorage.getItem("isDarkMode");
 
-if (localTheme) {
-  if (localTheme === "dark") {
-    body.classList.add(localTheme);
-    document.getElementById("theme").innerHTML = '<i class="fas fa-moon"></i>';
-  } else {
-    body.classList.add(localTheme);
-    document.getElementById("theme").innerHTML = '<i class="fas fa-sun"></i>';
-  }
-} else {
-  body.classList.add("light");
-  document.getElementById("theme").innerHTML = '<i class="fas fa-sun"></i>';
-  localStorage.setItem("theme", "light");
+if (isDarkMode) {
+  console.log(isDarkMode);
+  body.classList.add("dark");
+  document.getElementById("theme").innerHTML = '<i class="fas fa-moon"></i>';
 }
+
+const toggleDarkMode = () => {
+  body.classList.add("dark");
+  document.getElementById("theme").innerHTML = '<i class="fas fa-moon"></i>';
+  setTimer();
+  localStorage.setItem("isDarkMode", true);
+};
+
+const toggleLightMode = () => {
+  body.classList.remove("dark");
+  document.getElementById("theme").innerHTML = '<i class="fas fa-sun"></i>';
+  setTimer();
+  localStorage.removeItem("isDarkMode");
+};
+
+const setTimer = () => {
+  setTimeout(() => {
+    themeToggle.classList.remove("theme__toggle--active");
+  }, 900);
+};
 
 themeToggle.onclick = () => {
   themeToggle.classList.add("theme__toggle--active");
-  if (body.className === "light") {
-    body.classList.replace("light", "dark");
-    document.getElementById("theme").innerHTML = '<i class="fas fa-moon"></i>';
-    setTimeout(() => {
-      themeToggle.classList.remove("theme__toggle--active");
-    }, 900);
-    localStorage.setItem("theme", "dark");
+  if (body.className === "dark") {
+    toggleLightMode();
   } else {
-    body.classList.replace("dark", "light");
-    document.getElementById("theme").innerHTML = '<i class="fas fa-sun"></i>';
-    setTimeout(() => {
-      themeToggle.classList.remove("theme__toggle--active");
-    }, 900);
-    localStorage.setItem("theme", "light");
+    toggleDarkMode();
   }
 };
