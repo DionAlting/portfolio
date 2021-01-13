5425f98caf49285713fac301b837417d6d5db577
Dion <dion_a@live.nl>
2021-01-13 01:27:56 +0100

    Added profile images

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const themeToggle = document.querySelector(".theme__toggle");
 const theme = document.querySelector(".light");
+const profileImage = document.querySelector(".about-me__img");
 const body = document.body;
 
 const isDarkMode = localStorage.getItem("isDarkMode");
@@ -7,11 +8,13 @@ const isDarkMode = localStorage.getItem("isDarkMode");
 if (isDarkMode) {
   console.log(isDarkMode);
   body.classList.add("dark");
+  profileImage.classList.add("about-me__img--dark");
   document.getElementById("theme").innerHTML = '<i class="fas fa-moon"></i>';
 }
 
 const toggleDarkMode = () => {
   body.classList.add("dark");
+  profileImage.classList.add("about-me__img--dark");
   document.getElementById("theme").innerHTML = '<i class="fas fa-moon"></i>';
   setTimer();
   localStorage.setItem("isDarkMode", true);
@@ -19,6 +22,7 @@ const toggleDarkMode = () => {
 
 const toggleLightMode = () => {
   body.classList.remove("dark");
+  profileImage.classList.remove("about-me__img--dark");
   document.getElementById("theme").innerHTML = '<i class="fas fa-sun"></i>';
   setTimer();
   localStorage.removeItem("isDarkMode");
